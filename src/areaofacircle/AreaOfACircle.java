6d1e2ec8458d2d89dc01f488b5f0fc99ce69287f
eljam3239 <eljam3239@7KTM6Y1.Student.UGDSB.ED>
2017-02-28 13:10:16 -0500

    area of a circle with a radius of 15cm

---
+++ src/areaofacircle/AreaOfACircle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package areaofacircle;
+
+/**
+ *
+ * @author eljam3239
+ */
+public class AreaOfACircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double radius = 15;
+        double area = 3.14*radius*radius;
+        System.out.println(area);
+     
+    }
+    
+}
